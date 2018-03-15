637179266e9c10bc5f089bd1ac7ac44f988425ac
chavezoscar <chavezoscar@mail.fresnostate.edu>
2018-03-14 21:50:51 -0700

    updated for chapter 3

--- app/src/main/java/com/example/oscarchavez/littquiz/QuizActivity.java
+++ app/src/main/java/com/example/oscarchavez/littquiz/QuizActivity.java
@@ -3,6 +3,7 @@ package com.example.oscarchavez.littquiz;
 import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.*;
@@ -18,6 +19,10 @@ public class QuizActivity extends AppCompatActivity {
     private ImageButton mNextButton;
     private ImageButton mPrevButton;
     private TextView mQuestionTextView;
+
+    private static final String TAG = "QuizActivity";
+    private static final String KEY_INDEX= "index";
+
 
 
     //private char buttonChar;
@@ -45,7 +50,14 @@ public class QuizActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate(Bundle) called");
         setContentView(R.layout.activity_quiz);
+
+        if (savedInstanceState != null){
+            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+        }
+
+
 
 
         mQuestionTextView = findViewById(R.id.question_text_view);
@@ -158,5 +170,45 @@ public class QuizActivity extends AppCompatActivity {
 
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
 
+    }
+
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        Log.d(TAG, "onStart() Called");
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        Log.d(TAG, "onResume() Called");
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        Log.d(TAG, "onPause() Called");
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState){
+        super.onSaveInstanceState(savedInstanceState);
+        Log.i(TAG, "onSaveInstanceState");
+        savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
+
+
+    }
+
+    @Override
+    public void onStop(){
+        super.onStop();
+        Log.d(TAG, "onStop() Called");
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() Called");
     }
 }
